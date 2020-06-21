743db9b0bc2e5bac4e130b64ff098d305001b797
koheiinaba <kouhei0419.s.s@gmail.com>
2020-06-21 10:33:57 +0000

    rspecバリデーション変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Lorem.characters(number:10) }
     sequence(:email) { |n| "other#{n}@example.com" }
-    password { 'password' }
-    password_confirmation { 'password' }
+    password { '111111' }
+    password_confirmation { '111111' }
   end
 end
